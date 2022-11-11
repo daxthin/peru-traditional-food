6718896b931e9f6aacc0ee65977f359208ad2b23
daxthin <nicolasdga.dev@gmail.com>
2022-11-11 16:51:05 -0300

    deploy changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://daxthin.github.io'
+    site: 'https://daxthin.github.io',
+    base: '/landing-page-peru'
 });
